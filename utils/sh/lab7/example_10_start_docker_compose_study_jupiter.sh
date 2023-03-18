a69f91acc0fa7056c944c26faa6dd684c58856e8
Valerii Savchenko <wellic72@gmail.com>
2023-03-18 15:28:13 +0200

    Updated samples for jnb in lab7

--- utils/sh/lab7/example_10_start_docker_compose_study_jupiter.sh
+++ utils/sh/lab7/example_10_start_docker_compose_study_jupiter.sh
@@ -4,5 +4,6 @@ set -u;
 #set -x;
 
 docker-compose down
+docker-compose build study_bd_jupyter
 docker-compose up -d study_bd_jupyter
-google-chrome http://127.0.0.1:8888/lab/tree/sample.ipynb
+google-chrome http://127.0.0.1:8888/lab/tree/samples/sample.ipynb
--- utils/sh/lab7/example_20_start_docker_cli_study_jupiter.sh
+++ utils/sh/lab7/example_20_start_docker_cli_study_jupiter.sh
@@ -3,5 +3,7 @@
 set -u;
 #set -x;
 
-docker build -t study_bd_jupyter:latest -f Dockerfile.study_bd_jupyter .
-docker run --rm --name study_bd_jupyter  -p 8888:8888 -v $PWD:/app study_bd_jupyter:latest
+docker build -f Dockerfile.study_bd_jupyter -t study_bd_jupyter:latest .
+docker run -d --rm --name study_bd_jupyter -p 8888:8888 -v $PWD:/app study_bd_jupyter:latest
+google-chrome http://127.0.0.1:8888/lab/tree/samples/sample.ipynb
+
